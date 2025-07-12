fac7834b0328d4e0e3a1f549036014aae9df04d5
Niair <niair2180@gmail.com>
2025-07-12 17:03:27 +0530

    second_commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHON_e_DOT = '-e .'
+def get_requirements(file_path:str)-> List[str]:
+      '''
+      This function will return the list of all the requirements.
+      '''
+      requirements = []
+      with open(file_path) as obj:
+            requirements = obj.readlines()
+            requirements = [i.replace("\n", "") for i in requirements]
+            
+            if HYPHON_e_DOT in requirements:
+                  requirements.remove(HYPHON_e_DOT)
+            
+            return requirements
+
+
+
+setup(
+      name = 'Customer_Churn',
+      version = '0.0.1',
+      author = 'Nihal',
+      author_email = 'niair2180@gmail.com',
+      packages = find_packages(),
+      install_requires = get_requirements('requirements.txt')
+)
