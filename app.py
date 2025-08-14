1f6b074fde5f6e5d6cc9387c86a83a2f734b1c35
Niair <niair2180@gmail.com>
2025-08-14 19:07:53 +0530

    deploy....Part7

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 st.set_page_config(page_title="Customer Churn Predictor", page_icon="📉", layout="centered")
 
 os.environ["STREAMLIT_SERVER_PORT"] = "7860"
-os.environ["STREAMLIT_SERVER_ADDRESS"] = "127.0.0.1"
+os.environ["STREAMLIT_SERVER_ADDRESS"] = "0.0.0.0"
 os.environ["STREAMLIT_SERVER_HEADLESS"] = "true"
 os.environ["STREAMLIT_BROWSER_GATHER_USAGE_STATS"] = "false"
 
--- dockerfile
+++ dockerfile
@@ -24,4 +24,4 @@ ENV PORT=$PORT
 ENV STREAMLIT_SERVER_HEADLESS=true
 
 # Use PORT environment variable in command
-CMD ["sh", "-c", "streamlit run app.py --server.address=127.0.0.1 --server.port=$PORT"]
+CMD ["sh", "-c", "streamlit run app.py --server.address=0.0.0.0 --server.port=$PORT"]
